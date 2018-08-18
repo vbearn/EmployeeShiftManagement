a43d8e71df4181c710ea47e61997c3c9dcf016c0
Mohammad Nakhaie <mmncompany@gmail.com>
2018-08-18 14:41:27 +0430

    added ability to set rules dynamically

---
+++ Sources/BackEnd/CompanyX.EmployeeShiftManagement/ScheduleCalculator/Rules/ShiftRuleManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyX.EmployeeShiftManagement.ScheduleCalculator.Rules
+{
+
+    internal class ShiftRuleManager 
+    {
+
+        #region Fields
+
+        private List<ISchiftRuleBase> rules;
+        public void SetRules(List<ISchiftRuleBase> rules)
+        {
+            this.rules = rules;
+        }
+
+        #endregion
+
+        #region init
+
+        public ShiftRuleManager()
+        {
+           
+        }
+
+        #endregion
+
+        #region Reule Checking
+
+        public bool IsEmployeeEligibleForShift(EmployeeShiftItem currentPlanningShift, IReadOnlyList<EmployeeShiftItem> pastScheduleItemList)
+        {
+            foreach (var rule in this.rules)
+            {
+                if (!rule.SatisfiesRule(currentPlanningShift, pastScheduleItemList))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+    }
+
+}
