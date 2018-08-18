4a637e6d2f2370d18486c6e572fc6731e10bb16d
Mohammad Nakhaie <mmncompany@gmail.com>
2018-08-18 13:24:39 +0430

    Added rule for checking minimum 2 shifts for each employee in the scheduling period

--- Sources/BackEnd/CompanyX.EmployeeShiftManagement/ScheduleCalculator/Rules/IRuleBase.cs
+++ Sources/BackEnd/CompanyX.EmployeeShiftManagement/ScheduleCalculator/Rules/IRuleBase.cs
@@ -13,7 +13,8 @@ namespace CompanyX.EmployeeShiftManagement.ScheduleCalculator.Rules
             rules = new List<ISchdeuleRuleBase>() {
                 new OneShiftPerDayRule(),
                 new ConsecutiveAfternoonShiftsRule(),
-                new ConsecutiveDayShiftsEligibleForExemptionRule()
+                new ConsecutiveDayShiftsEligibleForExemptionRule(),
+                new EmployeeMinimumCompletedShiftsRule()
             };
         }
 
@@ -152,6 +153,45 @@ namespace CompanyX.EmployeeShiftManagement.ScheduleCalculator.Rules
                     return true;
                 }
             }
+
+        }
+
+    }
+
+    internal class EmployeeMinimumCompletedShiftsRule : ISchdeuleRuleBase
+    {
+        public bool SatisfiesRule(ScheduleShiftItem currentPlanningShift, IReadOnlyList<ScheduleShiftItem> pastScheduleItemList)
+        {
+            if (currentPlanningShift == null)
+                throw new ArgumentNullException("currentPlanningShift");
+
+            // if current employee has done two shifts or more, and there exists another employee 
+            // with less than two completed shifts, then we should give that employee a chance.
+            // so we disqualify current employee from taking another shift until all employees have performed at least 2 shifts.
+
+            var employeeCompletedShifts = pastScheduleItemList.Count(x => x.EmployeeId == currentPlanningShift.EmployeeId);
+            if (employeeCompletedShifts >= 2)
+
+
+            {
+                var otherEmployeesCompletedShifts = pastScheduleItemList.GroupBy(x => x.EmployeeId);
+
+                // if there exsits another employee with less than 2 completed shift
+                if (otherEmployeesCompletedShifts.Any(x => x.Count() < 2))
+                {
+                    // current employee is not eligible to take another shift until others perform their required shifts
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+
 
         }
 
